4d5e614fa2c0eb0ee32ad097f9e9aa74e1b20829
Gadi Cohen <dragon@wastelands.net>
2024-04-19 17:31:14 +0100

    mongoRelay: use DBNAME from const

--- src/pages/api/mongoRelay.ts
+++ src/pages/api/mongoRelay.ts
@@ -1,8 +1,9 @@
 import { MongoClient } from "mongodb";
 import makeExpressRelay from "mongodb-rest-relay/lib/express";
+import { DBNAME } from '../../api-lib/consts';
 
 const MONGO_URL = process.env.MONGO_URL || "mongodb://127.0.0.1";
 const client = new MongoClient(MONGO_URL);
 
 // ts fails sometimes here (but not always???) because of version mismatch
-export default makeExpressRelay((await client.connect()).db("shadowlang"));
+export default makeExpressRelay((await client.connect()).db(DBNAME));
